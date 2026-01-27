e9375e17183d220ce11b68187eaddf8855d1011f
yehia mohamed <yahiaagm470@gmail.com>
2026-01-27 22:38:17 +0200

    solve some logic errors in Validation.py

--- src/Validation.py
+++ src/Validation.py
@@ -1,44 +1,45 @@
-def ReadString(message:str) -> str:
-    x:str = input(message)
-    while(not x.isalpha):
-        x = input("Please Enter a Valid Type.")
+def ReadString(message: str) -> str:
+    x: str = input(message)
+    while not x.isalpha(): 
+        x = input("Please enter a valid string (letters only): ")
     return x
 
-def ReadInt(message:str) -> int:
-    isValid:bool = False
-    x_int:int = 0
+def ReadInt(message: str) -> int:
+    isValid: bool = False
+    x_int: int = 0
     while not isValid:
         try:
-            int(input(message))
-        except TypeError :
-            x = input("Please Enter a Valid Type.")
+            x_int = int(input(message))
+        except ValueError:
+            print("Please enter a valid integer.")
         else:
             isValid = True
     return x_int
-            
-def ReadFloat(message:str) -> float:
-    x:str = input(message)
-    isValid:bool = False
-    x_float:float = 0
-    while isValid:
+
+def ReadFloat(message: str) -> float:
+    isValid: bool = False
+    x_float: float = 0
+    while not isValid:
         try:
-            x_float = int(x)
-        except TypeError :
-            x = input("Please Enter a Valid Type.")
+            x_float = float(input(message)) 
+        except ValueError:
+            print("Please enter a valid float number.")
         else:
             isValid = True
     return x_float
 
-def ReadIntInRange(message:str, From:int, To:int) -> int:
-    num:int = ReadInt(message, f"({From}) : {To}")
-    while num > To and num < From:
-        print("Input Out Of Range .")
-        num = ReadInt(message, f"({From}) : {To}")
+def ReadIntInRange(message: str, From: int, To: int) -> int:
+    num: int = ReadInt(f"{message} ({From} - {To}): ")
+    while num < From or num > To: 
+        print("Input out of range.")
+        num = ReadInt(f"{message} ({From} - {To}): ")
     return num
 
-def ReadFloatInRange(message:str, From:float, To:float) -> float:
-    num:float = ReadInt(message, f"({From}) : {To}")
-    while num > To and num < From:
-        print("Input Out Of Range .")
-        num = ReadInt(message, f"({From}) : {To}")
+def ReadFloatInRange(message: str, From: float, To: float) -> float:
+    num: float = ReadFloat(f"{message} ({From} - {To}): ")
+    while num < From or num > To: 
+        print("Input out of range.")
+        num = ReadFloat(f"{message} ({From} - {To}): ")
     return num
+
+
