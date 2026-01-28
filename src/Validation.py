b8cf6a62eb8aa4924ee4edcf851e617810f06276
yehia mohamed <yahiaagm470@gmail.com>
2026-01-28 18:11:53 +0200

    some error handling in Validation.py

--- src/Validation.py
+++ src/Validation.py
@@ -1,4 +1,5 @@
 def ReadString(message: str) -> str:
+<<<<<<< Updated upstream
     x: str = input(message)
     while not x.isalpha(): 
         x = input("Please enter a valid string (letters only): ")
@@ -40,6 +41,50 @@ def ReadFloatInRange(message: str, From: float, To: float) -> float:
     while num < From or num > To: 
         print("Input out of range.")
         num = ReadFloat(f"{message} ({From} - {To}): ")
+=======
+    """Reads a string input containing only letters."""
+    x = input(message)
+    while not x.isalpha():
+        x = input("Please enter a valid string (letters only): ")
+    return x
+
+
+def ReadInt(message: str) -> int:
+    """Reads an integer input."""
+    while True:
+        try:
+            x_int = int(input(message))
+            return x_int
+        except ValueError:
+            print("Please enter a valid integer.")
+
+
+def ReadFloat(message: str) -> float:
+    """Reads a float input."""
+    while True:
+        try:
+            x_float = float(input(message))
+            return x_float
+        except ValueError:
+            print("Please enter a valid number.")
+
+
+def ReadIntInRange(message: str, From: int, To: int) -> int:
+    """Reads an integer input and ensures it's within a specified range."""
+    num = ReadInt(message + f" ({From}-{To}): ")
+    while num < From or num > To:
+        print("Input out of range.")
+        num = ReadInt(message + f" ({From}-{To}): ")
+    return num
+
+
+def ReadFloatInRange(message: str, From: float, To: float) -> float:
+    """Reads a float input and ensures it's within a specified range."""
+    num = ReadFloat(message + f" ({From}-{To}): ")
+    while num < From or num > To:
+        print("Input out of range.")
+        num = ReadFloat(message + f" ({From}-{To}): ")
+>>>>>>> Stashed changes
     return num
 
 
