869e72acca672f1fe1ed3b15185441dfc4068c4d
yehia mohamed <yahiaagm470@gmail.com>
2026-01-28 18:32:08 +0200

    submit the merge and solve conflict

--- src/Validation.py
+++ src/Validation.py
@@ -1,55 +1,4 @@
 def ReadString(message: str) -> str:
-<<<<<<< Updated upstream
-    x: str = input(message)
-    while not x.isalpha(): 
-        x = input("Please enter a valid string (letters only): ")
-    return x
-
-def ReadInt(message: str) -> int:
-    isValid: bool = False
-    x_int: int = 0
-    while not isValid:
-        try:
-            x_int = int(input(message))
-        except ValueError:
-            print("Please enter a valid integer.")
-        else:
-            isValid = True
-    return x_int
-
-def ReadFloat(message: str) -> float:
-    isValid: bool = False
-    x_float: float = 0
-    while not isValid:
-        try:
-            x_float = float(input(message)) 
-        except ValueError:
-            print("Please enter a valid float number.")
-        else:
-            isValid = True
-    return x_float
-
-def ReadIntInRange(message: str, From: int, To: int) -> int:
-    num: int = ReadInt(f"{message} ({From} - {To}): ")
-    while num < From or num > To: 
-        print("Input out of range.")
-        num = ReadInt(f"{message} ({From} - {To}): ")
-    return num
-
-<<<<<<< HEAD
-def ReadFloatInRange(message:str, From:float, To:float) -> float:
-    num:float = ReadInt(message, f"({From}) : {To}")
-    while num > To and num < From:
-        print("Input Out Of Range .")
-        num = ReadInt(message, f"({From}) : {To}")
-    return num
-=======
-def ReadFloatInRange(message: str, From: float, To: float) -> float:
-    num: float = ReadFloat(f"{message} ({From} - {To}): ")
-    while num < From or num > To: 
-        print("Input out of range.")
-        num = ReadFloat(f"{message} ({From} - {To}): ")
-=======
     """Reads a string input containing only letters."""
     x = input(message)
     while not x.isalpha():
@@ -92,8 +41,6 @@ def ReadFloatInRange(message: str, From: float, To: float) -> float:
     while num < From or num > To:
         print("Input out of range.")
         num = ReadFloat(message + f" ({From}-{To}): ")
->>>>>>> Stashed changes
     return num
 
 
->>>>>>> validation
